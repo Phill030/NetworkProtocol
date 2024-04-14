3e36f0523d2cb9f50f1f1b7ee961319fceabecca
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-14 23:03:55 +0200

    chore: add rustfmt

--- shared/src/encoder.rs
+++ shared/src/encoder.rs
@@ -3,10 +3,7 @@ use std::future::Future;
 use tokio::io::{AsyncWrite, AsyncWriteExt};
 
 pub trait Encoder {
-    fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> impl Future<Output = Result<(), EncodeError>> + Send;
+    fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> impl Future<Output = Result<(), EncodeError>> + Send;
 }
 
 pub trait EncoderWriteExt {
@@ -25,118 +22,79 @@ impl<W: AsyncWrite + Unpin> EncoderWriteExt for W {
 }
 
 impl Encoder for u8 {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         Ok(writer.write_u8(*self).await?)
     }
 }
 
 impl Encoder for i8 {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         Ok(writer.write_i8(*self).await?)
     }
 }
 
 impl Encoder for u16 {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         Ok(writer.write_u16(*self).await?)
     }
 }
 
 impl Encoder for i16 {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         Ok(writer.write_i16(*self).await?)
     }
 }
 
 impl Encoder for u32 {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         Ok(writer.write_u32(*self).await?)
     }
 }
 
 impl Encoder for i32 {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         Ok(writer.write_i32(*self).await?)
     }
 }
 
 impl Encoder for u64 {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         Ok(writer.write_u64(*self).await?)
     }
 }
 
 impl Encoder for i64 {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         Ok(writer.write_i64(*self).await?)
     }
 }
 
 impl Encoder for f32 {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         Ok(writer.write_f32(*self).await?)
     }
 }
 
 impl Encoder for f64 {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         Ok(writer.write_f64(*self).await?)
     }
 }
 
 impl Encoder for String {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         writer.write_string(self).await
     }
 }
 
 impl Encoder for &str {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         writer.write_string(self).await
     }
 }
 
 impl Encoder for bool {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         let val = match *self {
             true => 1,
             false => 0,
@@ -147,10 +105,7 @@ impl Encoder for bool {
 }
 
 impl Encoder for Vec<u8> {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         let len = self.len();
 
         writer.write_u32(len.try_into()?).await?;
@@ -159,10 +114,7 @@ impl Encoder for Vec<u8> {
 }
 
 impl Encoder for Vec<String> {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         writer.write_u32(self.len().try_into()?).await?;
 
         for val in self {
@@ -177,10 +129,7 @@ impl<const N: usize> Encoder for [u8; N]
 where
     [u8; N]: Sized,
 {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         writer.write_u32(self.len().try_into()?).await?;
         writer.write_all(self).await?;
 
@@ -189,10 +138,7 @@ where
 }
 
 impl<T: Encoder + Sync> Encoder for Option<T> {
-    async fn encode<W: AsyncWrite + Unpin + Send>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), EncodeError> {
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> Result<(), EncodeError> {
         if let Some(val) = self {
             val.encode(writer).await
         } else {
