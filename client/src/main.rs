dee8f62234496332e3c350499ab10d80687f877a
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-15 00:52:36 +0200

    feat(client): add basic connection

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io, net::TcpStream};
+
+use shared::{ADDR, PORT};
+
+fn main() -> io::Result<()> {
+    if let Ok(stream) = TcpStream::connect(format!("{ADDR}:{PORT}")) {
+        println!("> Connected to the server!");
+    } else {
+        panic!("> Couldn't connect to server...");
+    }
+
+    Ok(())
 }
