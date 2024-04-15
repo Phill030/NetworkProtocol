0aa834e80bd6021da8935a37f8fa9a11eceb7bd7
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-15 10:11:39 +0200

    feat(shared): add modules to lib

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,6 +1,13 @@
 pub mod decoder;
 pub mod encoder;
 pub mod errors;
+pub mod messages;
+pub mod types;
 
 pub const ADDR: &str = "127.0.0.1";
 pub const PORT: u16 = 7776;
+
+// Authentication flow:
+//
+//  AuthenticationRequest(Nonce): Server
+//  AuthenticationResponse(Hwid, Nonce): Client
