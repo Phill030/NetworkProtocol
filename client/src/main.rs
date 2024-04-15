be3839c501bed8cab7a55fa824ad58218cd2cb60
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-15 13:58:12 +0200

    test: add encoding & decoding Test using tokio runtime

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,10 @@
 use machineid_rs::{Encryption, HWIDComponent, IdBuilder};
-use shared::{ADDR, PORT};
-use std::{io, net::TcpStream};
+use shared::{encoder::Encoder, messages::client::AuthenticationResponse, types::Hwid, ADDR, PORT};
+use std::{
+    io::{self, Cursor},
+    net::TcpStream,
+};
+use tokio::runtime::Runtime;
 
 const KEY: &str = "HASHING_KEY";
 
@@ -13,6 +17,19 @@ fn main() -> io::Result<()> {
         .add_component(HWIDComponent::SystemID)
         .build(KEY)
         .unwrap();
+
+    Runtime::new().unwrap().spawn(async move {
+        let response = AuthenticationResponse {
+            hwid: Hwid { cpu_id, system_id },
+            nonce: "ABC".to_string(),
+        };
+
+        let v = Vec::with_capacity(1024);
+        let mut cursor = Cursor::new(v);
+
+        response.encode(&mut cursor).await.unwrap();
+        println!("Encoded {cursor:?}");
+    });
 
     if let Ok(stream) = TcpStream::connect(format!("{ADDR}:{PORT}")) {
         println!("> Connected to the server!");
