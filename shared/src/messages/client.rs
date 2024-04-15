65b018d6185478bfe898d92bd0d7a6a20139aa9f
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-15 13:57:08 +0200

    feat: add Serialize and Clone traits

--- shared/src/messages/client.rs
+++ shared/src/messages/client.rs
@@ -1,4 +1,9 @@
+use crate::decoder::Decoder;
+use crate::encoder::Encoder;
 use crate::types::Hwid;
+use macros::{Receivable, Serialize};
+
+#[derive(Serialize, Clone, Receivable)]
 
 pub struct AuthenticationResponse {
     pub hwid: Hwid,
--- shared/src/messages/server.rs
+++ shared/src/messages/server.rs
@@ -1,3 +1,7 @@
+use crate::encoder::Encoder;
+use macros::Serialize;
+
+#[derive(Serialize, Clone)]
 pub struct AuthenticationRequest {
     pub nonce: String,
 }
