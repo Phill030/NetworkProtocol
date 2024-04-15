41131043a3b2d78dfb1c190ea3378536b5f731cd
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-15 10:10:51 +0200

    feat(client): add cpu_id & system_id

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,19 @@
+use machineid_rs::{Encryption, HWIDComponent, IdBuilder};
+use shared::{ADDR, PORT};
 use std::{io, net::TcpStream};
 
-use shared::{ADDR, PORT};
+const KEY: &str = "HASHING_KEY";
 
 fn main() -> io::Result<()> {
+    let cpu_id = IdBuilder::new(Encryption::SHA256)
+        .add_component(HWIDComponent::CPUID)
+        .build(KEY)
+        .unwrap();
+    let system_id = IdBuilder::new(Encryption::SHA256)
+        .add_component(HWIDComponent::SystemID)
+        .build(KEY)
+        .unwrap();
+
     if let Ok(stream) = TcpStream::connect(format!("{ADDR}:{PORT}")) {
         println!("> Connected to the server!");
     } else {
