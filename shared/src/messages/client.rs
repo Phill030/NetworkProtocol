4335cd895d0ce92017ff420f4c28a4e65f7dc613
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-17 00:25:45 +0200

    feat(client): add client packets

--- shared/src/messages/client.rs
+++ shared/src/messages/client.rs
@@ -1,10 +1,23 @@
-use crate::{decoder::Decoder, encoder::Encoder, types::Hwid};
-use macros::{Receivable, Streamable};
-use tokio::io::{AsyncWrite, AsyncWriteExt};
+use crate::{decoder::Decoder, encoder::Encoder, message_type, messages::EncodeError, types::Hwid};
+use macros::Networked;
 
-#[derive(Streamable, Clone, Debug, Receivable)]
-#[packet_id(0x66)]
+#[derive(Debug)]
+pub enum ClientPackets {
+    AuthenticationResponse(AuthenticationResponse),
+    KeepAliveResponse(KeepAliveResponse),
+}
+
+message_type!(ClientMessageType; AuthenticationResponse, KeepAliveResponse);
+
+#[derive(Networked, Clone, Debug)]
+#[packet_id(0x00)]
 pub struct AuthenticationResponse {
     pub hwid: Hwid,
     pub nonce: String,
+}
+
+#[derive(Networked, Clone, Debug)]
+#[packet_id(0x01)]
+pub struct KeepAliveResponse {
+    timestamp: i64,
 }
