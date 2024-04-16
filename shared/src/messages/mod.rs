881c15f4208df8ebb7ce106adfc58a81ce18472f
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-17 00:27:53 +0200

    feat(shared): add message_type macro

--- shared/src/messages/mod.rs
+++ shared/src/messages/mod.rs
@@ -1,2 +1,28 @@
+use crate::errors::encode::EncodeError;
+
 pub mod client;
 pub mod server;
+
+pub trait SystemPacket {
+    async fn to_bytes(&self) -> Result<Vec<u8>, EncodeError>;
+}
+
+#[macro_export]
+macro_rules! message_type {
+    ($name:ident; $($variant:ident),*) => {
+        #[derive(PartialEq, Debug)]
+        pub enum $name {
+            $($variant),*,
+            InvalidEvent,
+        }
+
+        impl From<u8> for $name {
+            fn from(value: u8) -> Self {
+                match value {
+                    $(x if x == $name::$variant as u8 => $name::$variant),*,
+                    _ => $name::InvalidEvent,
+                }
+            }
+        }
+    };
+}
