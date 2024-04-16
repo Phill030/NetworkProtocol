1650c4ea2644a97b59ab1e9055dea63cc0d9fd65
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-17 00:31:31 +0200

    feat(server): add handling of reading and keeping alive

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,37 +1,98 @@
-use crate::client::Client;
-use shared::{ADDR, PORT};
-use std::{
-    io::{self, Read},
-    net::TcpListener,
+use shared::{
+    decoder::ReceiveFromStream,
+    messages::{
+        client::{AuthenticationResponse, ClientMessageType},
+        server::{AuthenticationRequest, KeepAliveRequest, ServerPackets},
+        SystemPacket,
+    },
+    ADDR, PORT,
 };
-use tokio::runtime::Runtime;
+use std::{
+    io::{self, Cursor},
+    net::SocketAddr,
+    time::Duration,
+};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{tcp::OwnedReadHalf, TcpListener},
+    spawn,
+    sync::mpsc::{channel, Sender},
+};
 
-mod client;
-
-fn main() -> io::Result<()> {
-    let rt = Runtime::new()?;
-    let listener = TcpListener::bind(format!("{ADDR}:{PORT}"))?;
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind(format!("{ADDR}:{PORT}")).await?;
 
     loop {
-        let (stream, addr) = listener.accept()?;
+        let (stream, addr) = listener.accept().await?;
+        let (reader, mut writer) = stream.into_split();
+        let (sender, mut receiver) = channel::<ServerPackets>(100);
+        let keep_alive_sender = sender.clone();
 
-        println!("> {} connected", stream.peer_addr()?);
-        rt.spawn(async move { handle_client(Client::new(stream, addr)).await });
+        spawn(async move { handle_client(addr, reader, sender).await });
+        spawn(async move { keep_alive(keep_alive_sender).await });
+
+        while let Some(recv) = receiver.recv().await {
+            let buffer = match recv {
+                ServerPackets::AuthenticationRequest(x) => x.to_bytes().await,
+                ServerPackets::KeepAliveRequest(x) => x.to_bytes().await,
+                _ => panic!("Invalid packet received!"),
+            }
+            .unwrap();
+
+            writer.write_all(&buffer[..]).await.unwrap();
+        }
     }
 }
 
-async fn handle_client(mut client: Client) -> io::Result<()> {
-    let mut buffer = [0; 1024];
+async fn handle_client(addr: SocketAddr, mut reader: OwnedReadHalf, sender: Sender<ServerPackets>) -> io::Result<()> {
+    sender
+        .send(ServerPackets::AuthenticationRequest(AuthenticationRequest::new()))
+        .await
+        .unwrap();
 
     loop {
-        let bytes_read = client.stream.read(&mut buffer)?;
-        if bytes_read == 0 {
-            println!("> {} disconnected", client.addr);
+        let len = reader.read_i32().await.unwrap_or(0) as usize;
+        if len == 0 {
+            println!("> {} disconnected", addr);
             break;
+        } // TODO Read chunks if more than 1024 bytes!!
+
+        let mut buffer = vec![0u8; len];
+        match reader.read_exact(&mut buffer).await {
+            Ok(read) => {
+                if read == 0 {
+                    println!("> {} disconnected", addr);
+                    break;
+                }
+
+                match ClientMessageType::from(buffer[0]) {
+                    ClientMessageType::AuthenticationResponse => {
+                        println!("{buffer:?}");
+                    }
+                    _ => panic!("Received invalid packet"),
+                }
+
+                println!("{read}");
+            }
+            Err(why) => panic!("{why}"),
         }
 
         println!("{}", String::from_utf8_lossy(&buffer[..]));
     }
 
     Ok(())
+}
+
+async fn keep_alive(sender: Sender<ServerPackets>) {
+    println!("Starting KeepAlive thread...");
+
+    let mut interval_timer = tokio::time::interval(Duration::from_secs(5));
+
+    loop {
+        interval_timer.tick().await;
+        let packet = ServerPackets::KeepAliveRequest(KeepAliveRequest::new());
+        println!("KeepAliveRequest sent... {:?}", packet);
+        sender.send(packet).await.unwrap();
+    }
 }
