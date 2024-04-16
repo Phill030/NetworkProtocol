c32836c558d17b347f12ea121cca32c54f469964
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-17 00:32:01 +0200

    feat(client): add basic handling of reading and writing

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,15 +1,58 @@
 use machineid_rs::{Encryption, HWIDComponent, IdBuilder};
-use shared::{decoder::ReceiveFromStream, encoder::SendToWriter, messages::client::AuthenticationResponse, types::Hwid, ADDR, PORT};
-use std::{
-    io::{self, Cursor, Seek},
-    thread::sleep,
-    time::Duration,
+use shared::{
+    messages::{
+        client::{AuthenticationResponse, ClientPackets},
+        server::ServerMessageType,
+        SystemPacket,
+    },
+    types::Hwid,
+    ADDR, PORT,
 };
-use tokio::runtime::Runtime;
+use std::{
+    io::{self},
+    process,
+};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{
+        tcp::{OwnedReadHalf, OwnedWriteHalf},
+        TcpStream,
+    },
+    spawn,
+    sync::mpsc::{channel, Receiver, Sender},
+};
 
 const KEY: &str = "HASHING_KEY";
 
-fn main() -> io::Result<()> {
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    if let Ok(stream) = TcpStream::connect(format!("{ADDR}:{PORT}")).await {
+        println!("> Connected to server!");
+
+        let (reader, writer) = stream.into_split();
+        let (sender, receiver) = channel::<ClientPackets>(100);
+        let chat_sender = sender.clone();
+
+        spawn(async move { read_messages(reader, sender).await });
+        spawn(async move { write_messages(writer, receiver).await });
+
+        loop {
+            let mut input = String::new();
+            io::stdin().read_line(&mut input)?;
+
+            let trimmed_input = input.trim();
+            if trimmed_input.is_empty() {
+                continue;
+            }
+
+            // TODO: Send text
+        }
+    } else {
+        panic!("> Couldn't connect to server...");
+    }
+}
+
+pub async fn read_messages(mut reader: OwnedReadHalf, sender: Sender<ClientPackets>) {
     let cpu_id = IdBuilder::new(Encryption::SHA256)
         .add_component(HWIDComponent::CPUID)
         .build(KEY)
@@ -19,29 +62,59 @@ fn main() -> io::Result<()> {
         .build(KEY)
         .unwrap();
 
-    Runtime::new().unwrap().spawn(async move {
-        let response = AuthenticationResponse {
-            hwid: Hwid { cpu_id, system_id },
-            nonce: "ABC".to_string(),
-        };
+    loop {
+        let len = reader.read_i32().await.unwrap_or(0) as usize;
+        if len == 0 {
+            println!("Server disconnected");
+            process::exit(0);
+        }
 
-        let v = vec![];
-        let mut cursor = Cursor::new(v);
+        let mut buffer = vec![0u8; len];
+        match reader.read_exact(&mut buffer).await {
+            Ok(read) => {
+                if read == 0 {
+                    println!("Server disconnected");
+                    process::exit(0);
+                }
 
-        response.send(&mut cursor).await.unwrap();
-        println!("Encoded {cursor:?}");
+                match ServerMessageType::from(buffer[0]) {
+                    ServerMessageType::AuthenticationRequest => {
+                        println!("Received AuthenticationRequest");
 
-        let rec = AuthenticationResponse::receive(&mut cursor).await.unwrap();
-        println!("{rec:?}");
-    });
+                        sender
+                            .send(ClientPackets::AuthenticationResponse(AuthenticationResponse {
+                                hwid: Hwid {
+                                    cpu_id: cpu_id.clone(),
+                                    system_id: system_id.clone(),
+                                },
+                                nonce: "ABC".to_string(),
+                            }))
+                            .await
+                            .unwrap();
+                    }
+                    ServerMessageType::KeepAliveRequest => {
+                        println!("Received KeepAliveRequest");
+                    }
+                    _ => panic!("Received invalid packet!"),
+                }
+                println!("{}", String::from_utf8_lossy(&buffer[..]));
 
-    // if let Ok(stream) = TcpStream::connect(format!("{ADDR}:{PORT}")) {
-    //     println!("> Connected to the server!");
-    // } else {
-    //     panic!("> Couldn't connect to server...");
-    // }
+                buffer.clear();
+            }
+            Err(why) => panic!("{why}"),
+        }
+    }
+}
 
-    std::thread::sleep(Duration::from_secs(12312312));
+pub async fn write_messages(mut writer: OwnedWriteHalf, mut receiver: Receiver<ClientPackets>) {
+    while let Some(recv) = receiver.recv().await {
+        let buffer = match recv {
+            ClientPackets::AuthenticationResponse(x) => x.to_bytes().await,
+            ClientPackets::KeepAliveResponse(x) => x.to_bytes().await,
+            _ => panic!("Invalid packet received!"),
+        }
+        .unwrap();
 
-    Ok(())
+        writer.write_all(&buffer[..]).await.unwrap();
+    }
 }
