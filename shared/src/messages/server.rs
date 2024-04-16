c53fcd56ebc6e7bf5c10737268beecde62a861b6
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-17 00:25:33 +0200

    feat: add Server packets

--- shared/src/messages/server.rs
+++ shared/src/messages/server.rs
@@ -1,7 +1,38 @@
-use crate::encoder::Encoder;
-use macros::Serialize;
+use crate::{decoder::Decoder, encoder::Encoder, message_type, messages::EncodeError};
+use macros::Networked;
+use std::time::{SystemTime, UNIX_EPOCH};
+use textnonce::TextNonce;
 
-#[derive(Serialize, Clone)]
+#[derive(Debug)]
+pub enum ServerPackets {
+    AuthenticationRequest(AuthenticationRequest),
+    KeepAliveRequest(KeepAliveRequest),
+}
+
+message_type!(ServerMessageType; AuthenticationRequest, KeepAliveRequest);
+
+#[derive(Networked, Clone, Debug)]
+#[packet_id(0x00)]
 pub struct AuthenticationRequest {
     pub nonce: String,
+}
+impl AuthenticationRequest {
+    pub fn new() -> Self {
+        Self {
+            nonce: TextNonce::sized(16).unwrap().into_string(),
+        }
+    }
+}
+
+#[derive(Networked, Clone, Debug)]
+#[packet_id(0x01)]
+pub struct KeepAliveRequest {
+    pub timestamp: i64,
+}
+impl KeepAliveRequest {
+    pub fn new() -> Self {
+        Self {
+            timestamp: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as i64,
+        }
+    }
 }
